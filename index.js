7d8b0134edd64b4912bc20d3255615b43d2bc214
Karthik SO <sonakarthik@outlook.com>
2021-12-06 18:36:44 +0530

    fix for console log error

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require("express");
 var app = express();
 
-app.get('/getflight', function (req, res) {
-    console.log("Get Flights Page")``;
-    res.send('Hello GET');
+app.get('/getflights', function (req, res) {
+    console.log("Get Flights Page");
+    res.send('Hello GET Flights -- getting the list from mongoDB');
  })
 
  app.get('/bookflight', function (req, res) {
     console.log("Book Flight Page");
-    res.send('Flights Booked');
+    res.send('Flights Booked in MongodB');
  })
 
 var server = app.listen(8081, function(){
     var host = server.address().address
-    var port = server.address().port
+    var port = server.address().port;
 
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("Example app listening at http://%s:%s", host, port);
 })
